0b71d738b18b50e03795ecb0d78c916018bc71d0
Anders <andreibors2003@gmail.com>
2026-03-11 02:38:15 +0100

    Production-like functionality for the search button && API link generation based in React function for routes usage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,13 +10,64 @@ function App() {
   // important as fuck -- every relationship between variable updated --> by method HAS TO BE under a useState
   // useState is basically a very sensible method which redraws the screen based on if any changes happens
 
-    const [searchQuery, setSearchQuery] = useState("");
+    // keep the parameters for the search API link call needed for data manipulation on server level
+    const [formData, setFormData] = useState({
+        origin: "",
+        destination: "",
+        departure_date: "",
+        transport_type: "plane" // default value
+    });
 
-    const runSearch = () => {
-        console.log("Searching for: ", searchQuery);
-  }
+    // Setter function, keeps data fields and updates the specific fields the user changes on the form
+    // Does not allow the user to generate search links without all the required parameters filled
+    const handleInputChange = (field, value) => {
+        setFormData(prev => ({
+            ...prev,
+            [field]: value
+        }));
+        // has to set back the search button to enabled when the user starts typing, as the fill the required fields
+        if (error) setError("");
+    };
 
-  return (
+    // run the search in the date base & and assure all required fields are filled before sending the API request
+    const [error, setError] = useState("");
+
+    const runSearch = async (e) => {
+        e.preventDefault();
+
+        // validating fields
+        if (!formData.origin.trim() || !formData.destination.trim()) {
+            setError("Please fill in all fields in order to search.");
+            return;
+        }
+        // if not triggered or new attempt with required fields filled --> message cleared:
+        setError("");
+
+        // constructing URL routes based on user input
+        const query = new URLSearchParams(formData).toString();
+        // very picky here, must always include the protocol, will NOT work otherwise
+        const finalURL = `https://api.andreiradxa.online/api/search?${query}`
+        console.log("Testing, fetching data from " + finalURL)
+
+        // API call and data fetch
+        try {
+            const response = await fetch(finalURL);
+
+            if (!response.ok) {
+                throw new Error(`Server responded with status: ${response.status}`);
+            }
+            const data = await response.json();
+            console.log("Database connection -- OK.");
+            console.log("Displaying fetched information:");
+            console.table(data);
+
+        } catch (err) {
+            console.error("Database connection failed: ", err.message);
+            setError("Could  not connect to the database. The backend server is just a little home-lab server -- it might be offline or intentionally turned off.")
+        }
+    };
+
+    return (
 <>
     {/* ZONE 3 -- here is where the HTML is store, this zone is the "Render on page space" */}
     {/* Header */}
@@ -49,8 +100,8 @@ function App() {
 
     <div className="container">
         <div className="booking-card">
-            <h2 className="title">Find Your Flight</h2>
-            <form className="form">
+            <h2 className="title">Find Your Destination</h2>
+            <form className="form" onSubmit={runSearch}>
                 <div className="form-grid">
                     <div className="input-group">
                         <label>From</label>
@@ -58,11 +109,14 @@ function App() {
                             <svg className="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/>
                             </svg>
-                            <select>
-                                <option>Arlanda (ARN)</option>
-                                <option>Göteborg</option>
-                                <option>Malmö</option>
-                            </select>
+                            {/*Input with autocompletion, sends arguments to route link parameters */}
+                            <input
+                                type="text"
+                                className=''
+                                placeholder='I will travel from...'
+                                value={formData.origin}
+                                onChange={(e) => handleInputChange('origin', e.target.value)}
+                            />
                         </div>
                     </div>
 
@@ -72,11 +126,13 @@ function App() {
                             <svg className="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/>
                             </svg>
-                            <select>
-                                <option>Tokyo(TKY)</option>
-                                <option>Paris (PAR)</option>
-                                <option>London (LNd)</option>
-                            </select>
+                            <input
+                                type="text"
+                                className=''
+                                placeholder='I will travel to...'
+                                value={formData.destination}
+                                onChange={(e) => handleInputChange('destination', e.target.value)}
+                            />
                         </div>
                     </div>
 
@@ -86,7 +142,11 @@ function App() {
                             <svg className="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
                             </svg>
-                            <input type="date"/>
+                            <input
+                                type='date'
+                                value={formData.departure_date}
+                                onChange={(e) => handleInputChange('departure_date', e.target.value)}
+                            />
                         </div>
                     </div>
 
@@ -115,19 +175,37 @@ function App() {
                     </div>
 
                     <div className="input-group">
-                        <label>Class</label>
+                        <label>Transportation</label>
                         <div className="input-box">
-                            <select>
-                                <option>Economy</option>
-                                <option>Business</option>
-                                <option>First Class</option>
+                            <select
+                            value={formData.transport_type}
+                            onChange={(e) => handleInputChange('transport_type', e.target.value)}
+                                >
+                                <option value="plane">Plane</option>
+                                <option value="bus">Bus</option>
+                                <option value="train">Train</option>
                             </select>
                         </div>
                     </div>
                 </div>
+                {/*UI error message in when required fields in the form are not filled in*/}
+                {error &&  (
+                    <p style={{ color: '#ff4d4d', fontSize: '0.8rem', margin: '1rem 0 1rem 0', textAlign: 'center'}}>
+                        {error}
+                    </p>
+                )}
 
-                <button type="submit" className="search-btn">
-                    Search Flights
+                <button
+                type="submit"
+                className="search-btn"
+                // Made to enforce search rules to not break the API link generation
+                disabled={error !== ""} // Will disabled the button in case the user triggers the field error, it ENFORCES that the search rules are respected in order to proceed further
+                style={{
+                    cursor: error ? 'not-allowed' : 'pointer',
+                    opacity: error ? 0.5 : 1
+                }}
+                >
+                    Search
                     <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"/>
                     </svg>
